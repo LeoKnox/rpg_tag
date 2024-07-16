f1799a07a2a4ef5f0222e72182f894d00daab099
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-16 15:54:14 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 export default Home = () => {
+  const playersState = [
+    { id: 1, gridArea: "4/5", hp: 18, type: "user", token: "赤", name: "Aka" },
+    { id: 2, gridArea: "4/4", hp: 18, type: "user", token: "青", name: "Ao" },
+    {
+      id: 3,
+      gridArea: "5/5",
+      hp: 18,
+      type: "user",
+      token: "緑",
+      name: "Midori",
+    },
+    {
+      id: 4,
+      gridArea: "5/4",
+      hp: 18,
+      type: "user",
+      token: "紫",
+      name: "Murasaki",
+    },
+  ];
   return (
     <div>
-  <h3>Home Page</h3>
-  <div className="column" style={{gridArea: '1/1/9/2'}}>
-    <div className="aDiv item">x</div>
-    <div className="bDiv item">x</div>
-    <div className="aDiv item">x</div>
-    <div className="bDiv item">x</div>
-    <div className="aDiv item">x</div>
-    <div className="bDiv item">x</div>
-    <div className="aDiv item">x</div>
-    <div className="bDiv item">x</div>
-  </div>
-  </div>
+      <h3>Home Page</h3>
+      <div className="column" style={{ gridArea: "1/1/9/2" }}>
+        <div className="aDiv item">x</div>
+        <div className="bDiv item">x</div>
+        <div className="aDiv item">x</div>
+        <div className="bDiv item">x</div>
+        <div className="aDiv item">x</div>
+        <div className="bDiv item">x</div>
+        <div className="aDiv item">x</div>
+        <div className="bDiv item">x</div>
+      </div>
+    </div>
   );
 };
