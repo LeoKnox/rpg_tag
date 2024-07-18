4ff7d76fd5d7957d9669c24f13ac02e9d58e17d2
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-17 19:45:23 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -29,12 +29,22 @@ export default Home = () => {
   return (
     <div>
       <h3>Home Page</h3>
-      <div className="column" style={gridStyle}>
-        <div className="aDiv item">x</div>
-        <div className="bDiv item">x</div>
-        <div className="aDiv item">x</div>
-        <div className="bDiv item">x</div>
-        <div className="aDiv item">x</div>
+      <div className="column">
+        <div className="aDiv item" style={gridStyle}>
+          x
+        </div>
+        <div className="bDiv item" style={gridStyle}>
+          x
+        </div>
+        <div className="aDiv item" style={gridStyle}>
+          x
+        </div>
+        <div className="bDiv item" style={gridStyle}>
+          x
+        </div>
+        <div className="aDiv item" style={gridStyle}>
+          x
+        </div>
         <div className="bDiv item">x</div>
         <div className="aDiv item">x</div>
         <div className="bDiv item">x</div>
