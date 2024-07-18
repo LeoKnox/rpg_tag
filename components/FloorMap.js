26a7f8d091f3090a8f099c683e77dcaef8c39b2a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-18 14:36:51 -0700

    Update FloorMap.js

--- components/FloorMap.js
+++ components/FloorMap.js
@@ -1,15 +1,25 @@
 export default FloorMap = () => {
   let roomOne = {
-    position: "relative",
+    position: "absolute",
     height: "100px",
     width: "75px",
     top: "150px",
     left: "60px",
+    border: "3px solid black",
+  };
+  let roomTwo = {
+    position: "absolute",
+    height: "100px",
+    width: "75px",
+    top: "50px",
+    left: "150px",
+    border: "3px solid green",
   };
   return (
     <>
       <p>Floor Map</p>
       <p style={roomOne}>h</p>
+      <p style={roomTwo}>h</p>
     </>
   );
 };
