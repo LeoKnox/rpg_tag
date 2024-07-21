c9d2ac2f17a5ca62bc266b1fd83e3f9d062dc1ee
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-20 18:05:11 -0700

    Update FloorMap.js

--- components/FloorMap.js
+++ components/FloorMap.js
@@ -1,16 +1,18 @@
+import { useState } from "react";
+
 export default FloorMap = () => {
   let level = {
     position: "absolute",
     border: "4px solid red",
   };
-  let roomOne = {
+  const [roomOne, setRoomOne] = useState({
     position: "absolute",
     height: "100px",
     width: "75px",
     top: "150px",
     left: "60px",
     border: "3px solid black",
-  };
+  });
   let roomTwo = {
     position: "absolute",
     height: "100px",
@@ -22,8 +24,15 @@ export default FloorMap = () => {
   const changeRoom = (x) => {
     let newVal = x + parseInt(roomOne.width.slice(0, -2));
 
-    roomOne.width = newVal + "px";
-    alert(newVal);
+    setRoomOne({
+      position: "absolute",
+      height: "100px",
+      width: "75px",
+      top: "150px",
+      left: "70px",
+      border: "3px solid black",
+    });
+    alert(JSON.stringify(roomOne));
   };
   return (
     <div style={level}>
