003fecee290eddb08f3f402959f849affb74002f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-28 13:42:16 -0700

    Update GridMap.js

--- components/GridMap.js
+++ components/GridMap.js
@@ -8,6 +8,9 @@ export default GridMap = () => {
     gridTemplateColumns: "repeat(20, 1fr)",
     color: useColor,
     aspectRatio: "1",
+    width: "600px",
+    height: "600px",
+    border: "1px solid lightblue",
   };
   const roomStyle1 = {
     border: "2px solid darkblue",
@@ -16,7 +19,7 @@ export default GridMap = () => {
   const roomStyle2 = {
     border: "2px solid darkblue",
     gridColumn: "5/ span 3",
-    gridRow: "8/ span 2",
+    gridRow: "8/ span 12",
   };
   return (
     <div style={gridMapStyle}>
