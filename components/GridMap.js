cf5c24e760b68148057ae0ee8a702de732c4d109
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-27 17:40:37 -0700

    Create GridMap.js

---
+++ components/GridMap.js
@@ -0,0 +1,11 @@
+export default GridMap = () => {
+  const gridMapStyle = {
+    border: "2px solid red",
+    color: "blue"
+  };
+  return (
+    <div style={ gridMapStyle }>
+      <p>Grid Map</p>
+    </div>
+  );
+};
