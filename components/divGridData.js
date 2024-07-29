1cdee9dc4881c3b315a23622267138ee3a935e2e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-29 12:47:52 -0700

    Update divGridData.js

--- components/divGridData.js
+++ components/divGridData.js
@@ -1,1 +1,4 @@
-export const divGridData = [{ x: 3, y: 2, width: 8, height: 7 }];
+export const divGridData = [
+  { id: 0, x: 3, y: 2, width: 8, height: 7 },
+  { id: 1, x: 5, y: 6, width: 10, height: 12 },
+];
