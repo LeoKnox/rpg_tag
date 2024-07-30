be40064809a7d80e2c9a62463d80f09f11ddcd8e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-30 12:36:45 -0700

    Update divGridData.js

--- components/divGridData.js
+++ components/divGridData.js
@@ -1,4 +1,9 @@
-export const divGridData = [
-  { id: 0, x: 3, y: 2, width: 8, height: 7, color: "darkgreen" },
-  { id: 1, x: 5, y: 6, width: 10, height: 12 },
+export let divGridData = [
+  { id: 0, x: 3, y: 2, width: 5, height: 5, color: "darkgreen" },
+  { id: 1, x: 5, y: 6, width: 5, height: 5 },
 ];
+
+export const changeMap = () => {
+  divGridData[0].width = 8;
+  return divGridData;
+};
