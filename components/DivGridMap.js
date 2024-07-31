999c68f8bc14b44bfb63cc4feb815ae65e661165
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-31 14:06:48 -0700

    Update DivGridMap.js

--- components/DivGridMap.js
+++ components/DivGridMap.js
@@ -1,3 +1,7 @@
-export default DivGridMap = () => {
-  return <div>a</div>;
+export default DivGridMap = ({ room }) => {
+    const roomStyle = {
+        border: `2px solid ${room.id}`,
+        gridArea: `${room.x}/${room.y}/ ${room.width}/ ${room.length}`,
+      };
+  return <div style={roomStyle}>{room.id}</div>;
 };
