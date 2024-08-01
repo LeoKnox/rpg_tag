b87aad89bfd54075b86e2c3cbc75676246f725c2
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-31 17:50:10 -0700

    Update GridMap.js

--- components/GridMap.js
+++ components/GridMap.js
@@ -11,8 +11,9 @@ export default GridMap = () => {
     id = 0;
     let newRoom = (room) => ({
       ...room,
-      ...(room.width = 8),
+      ...(room.width: 8),
     });
+    console.log("T");
     console.log(JSON.stringify(newRoom));
   };
 
