27bfb3584897e626fe0fa5146361e48414ad2a5c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-31 17:39:04 -0700

    Update GridMap.js

--- components/GridMap.js
+++ components/GridMap.js
@@ -9,58 +9,12 @@ export default GridMap = () => {
 
   const changeMap = (id) => {
     id = 0;
-    const newRoom = gridMap.map((room) => (room.id == id ? (...room.x= 8) : room)
-    //alert(`${gridMap[0].id} = ${id}`);
-    setGridMap(newRoom);
-  };import { useState } from "react";
-import { divGridData } from "./divGridData.js";
-import DivGradMap from "./DivGridMap.js";
-
-export default GridMap = () => {
-  const [useColor, setUseColor] = useState("blue");
-  const [dungeonOffset, setDungeonOffset] = useState(2);
-  const [gridMap, setGridMap] = useState(divGridData);
-
-  const changeMap = (id) => {
-    id = 0;
     const newRoom = gridMap.map((room) =>
       room.id == id ? (room.x = 8) : room
     );
     //alert(`${gridMap[0].id} = ${id}`);
     setGridMap(newRoom);
   };
-
-  const gridMapStyle = {
-    display: "grid",
-    gridTemplateColumns: "repeat(20, 1fr)",
-    gridTemplateRows: "repeat(20, 1fr)",
-    color: useColor,
-    aspectRatio: "1",
-    width: "600px",
-    height: "600px",
-    border: "1px solid lightblue",
-  };
-  const roomStyle1 = {
-    border: "2px solid darkblue",
-    //gridArea: `${gridMap[0].x}/  ${gridMap[0].y}/ ${gridMap[0].width}/ ${gridMap[0].height}`,
-  };
-  const roomStyle2 = {
-    border: "2px solid darkblue",
-    gridArea: "4/2/span 5/ span 5",
-  };
-  return (
-    <div>
-      <button onClick={changeMap}>change</button>
-      <div style={gridMapStyle}>
-        <DivGridMap room={gridMap[0]} />
-        {gridMap.map((room) => (
-          <DivGridMap room={room} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
 
   const gridMapStyle = {
     display: "grid",
