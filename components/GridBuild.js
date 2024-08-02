af7237710e3ff2ef4fba2d1c1037454c07d6df5e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-02 14:48:55 -0700

    Update GridBuild.js

--- components/GridBuild.js
+++ components/GridBuild.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import DivBridMap from "./DivGridMap.js";
 
 export default GridBuild = () => {
   const [width, setWidth] = useState(5);
+  const [height, setHeight] = useState(5);
+  const [x, setx] = useState(5);
+  const [y, sety] = useState(5);
+  let room = { width: 5, length: 5, x: 1, y: 3 };
+  const gridRoomStyle = {};
   const gridBuildStyle = {
     color: "slate",
     width: "5ch",
@@ -18,6 +24,7 @@ export default GridBuild = () => {
         <label>y: </label>
         <input type="number" />
       </div>
+      <DivGridMap room={room} />
     </div>
   );
 };
