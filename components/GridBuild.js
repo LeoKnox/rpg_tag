136277ab3e94296678f79664d5ee2f0f3afa1e80
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-02 14:16:59 -0700

    Update GridBuild.js

--- components/GridBuild.js
+++ components/GridBuild.js
@@ -1,12 +1,13 @@
 export default GridBuild = () => {
   const gridBuildStyle = {
     color: "slate",
+    width: "5ch",
   };
   return (
     <div>
       <div style={gridBuildStyle}>
         <label>Width: </label>
-        <input type="number" />
+        <input type="number" size="3" />
         <label>Height: </label>
         <input type="number" />
         <label>x: </label>
