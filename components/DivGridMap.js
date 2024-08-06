abd4a2b5efce209d19eae752b3504241f7e73f33
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-06 16:15:42 -0700

    Update DivGridMap.js

--- components/DivGridMap.js
+++ components/DivGridMap.js
@@ -1,4 +1,4 @@
-export default DivGridMap = ({ room }, handleMouseDown) => {
+export default DivGridMap = ({ room, handleMouseDown }) => {
   const roomStyle = {
     border: `2px solid ${room.color || "darkblue"}`,
     gridArea: `${room.y}/${room.x}/ span ${room.height}/ span ${room.width}`,
