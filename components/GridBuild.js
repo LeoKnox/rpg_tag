27f76d18fd0c9fe20520fe8ac648255d6a3a3481
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-11 11:37:19 -0700

    Update GridBuild.js

--- components/GridBuild.js
+++ components/GridBuild.js
@@ -13,8 +13,9 @@ export default GridBuild = () => {
   const [initialXPosition, setInitialXPosition] = useState(0);
   const [initialYPosition, setInitialYPosition] = useState(0);
   useEffect(() => {
+    console.log("current room");
     setDivGridData(roomData(xOffset,yOffset))
-  },[])
+  },[currentRoom])
   useEffect(() => {
     
   setCurrentRoom(divGridData[currentRoomId]);
