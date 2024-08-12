6e64f46abe8c9ff7ed32af1a036561dd141f8b5b
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-12 11:02:04 -0700

    Update divGridData.js

--- components/divGridData.js
+++ components/divGridData.js
@@ -1,11 +1,15 @@
 export let divGridData = [
   { id: 0, x: 3, y: 2, width: 5, height: 5, color: "darkgreen" },
-  { id: 1, x: 5, y: 6, width: 5, height: 5 },
+  { id: 1, x: 5, y: 6, width: 6, height: 6 },
 ];
 
-export const roomData = (offsetX = 10, offsetY) => {
+export const roomData = (offsetX, offsetY) => {
   const x = divGridData.map((room) => {
-    return { ...room, x: room.x + offsetX, y: room.y + offsetY };
+    return {
+      ...room,
+      x: parseInt(room.x) + parseInt(offsetX),
+      y: room.y + offsetY,
+    };
   });
   return x;
 };
