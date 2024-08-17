1b9e8c766418400d8e31dab1e1535ec686a4a6db
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-16 17:08:37 -0700

    Update GridBuild.js

--- components/GridBuild.js
+++ components/GridBuild.js
@@ -69,7 +69,7 @@ export default GridBuild = () => {
   return (
     <div>
       <div style={gridBuildStyle}>
-        <p>X:{xOffset} Y:{yOffset} id:<input type="number" value={currentRoomId} /></p>
+        <p>X:{xOffset} Y:{yOffset} id:<input type="number" value={currentRoomId} onChange={(e) => setCurrentRoomId(e.target.value)}/></p>
         <label>Width: </label>
         <input
           type="number"
