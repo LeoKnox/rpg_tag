6d5ec2d90f9a9d224e4d28f541f6c4c623e35e8c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-18 15:48:34 -0700

    Update ArrowDiv.js

--- components/ArrowDiv.js
+++ components/ArrowDiv.js
@@ -1,8 +1,16 @@
 export default ArrowDiv = ({ currentRoom, setCurrentRoom }) => {
   return (
     <div style={{ display: "flex", color: "red" }}>
-      <div onClick={() => setCurrentRoom({ ...currentRoom, x: 1 })}>Left</div>
-      <div>Right</div>
+      <div
+        onClick={() => setCurrentRoom({ ...currentRoom, x: currentRoom.x - 1 })}
+      >
+        Left
+      </div>
+      <div
+        onClick={() => setCurrentRoom({ ...currentRoom, x: currentRoom.x + 1 })}
+      >
+        Right
+      </div>
     </div>
   );
 };
