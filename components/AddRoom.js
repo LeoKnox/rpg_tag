123bfc9916fe3d1d16754e100df781fe284d7f01
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-19 16:09:19 -0700

    Update AddRoom.js

--- components/AddRoom.js
+++ components/AddRoom.js
@@ -7,15 +7,38 @@ export default AddRoom = ({ addRoom }) => {
     x: 0,
     y: 1,
   });
+  const changeData = (e) => {
+    let temp = { ...newRoom, [e.target.name]: e.target.value };
+    setNewRoom(temp);
+  };
   return (
     <>
       <div style={{ display: "flex" }}>
         <label>Width:</label>
         <input
           type="number"
+          name="width"
           value={newRoom.width}
-          onChange={(e) => {...setNewRoom, width:(e.target.value)}}
+          onChange={changeData}
         />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>Height:</label>
+        <input
+          type="number"
+          name="height"
+          value={newRoom.height}
+          onChange={changeData}
+        />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>X:</label>
+        <input type="number" name="x" value={newRoom.x} onChange={changeData} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>Y:</label>
+        <input type="number" name="y" value={newRoom.y} onChange={changeData} />
+        <ArrowDiv param="y" currentRoom={newRoom} setCurrentRoom={setNewRoom} />
       </div>
       <button onClick={() => addRoom(newRoom)}>Add</button>
     </>
