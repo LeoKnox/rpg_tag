e61d9439dbf437b116b2a4b7e85f5b20b6fb1e81
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-19 12:30:00 -0700

    Update AddRoom.js

--- components/AddRoom.js
+++ components/AddRoom.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
-export default AddRoom = () => {
+export default AddRoom = ({ addRoom }) => {
   const [newRoom, setNewRoom] = useState({
-    width: 0,
-    height: 0,
+    width: 5,
+    height: 5,
     x: 0,
-    y: 0,
+    y: 1,
   });
   return (
     <div style={{ display: "flex" }}>
       <label>Width:</label>
       <input type="number" value={newRoom.width} />
+      <button onClick={addRoom}>Add</button>
     </div>
   );
 };
