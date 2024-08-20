7a98495810b7affc675e64c447977c18eb6c9138
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-20 12:03:38 -0700

    Create EditRoom.js

---
+++ components/EditRoom.js
@@ -0,0 +1,47 @@
+export default EditRoom = (currentRoom) => {
+  const changeData = () => {
+    console.log("dc");
+  };
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <label>Width:</label>
+        <input
+          type="number"
+          name="width"
+          value={currentRoom.width}
+          onChange={changeData}
+        />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>Height:</label>
+        <input
+          type="number"
+          name="height"
+          value={currentRoom.height}
+          onChange={changeData}
+        />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>X:</label>
+        <input
+          type="number"
+          name="x"
+          value={currentRoom.x}
+          onChange={changeData}
+        />
+      </div>
+      <div style={{ display: "flex" }}>
+        <label>Y:</label>
+        <input
+          type="number"
+          name="y"
+          value={currentRoom.y}
+          onChange={changeData}
+        />
+        <ArrowDiv param="y" currentRoom={currentRoom} />
+      </div>
+      <button onClick={() => addRoom(newRoom)}>Edit</button>
+    </>
+  );
+};
