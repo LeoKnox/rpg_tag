15e020897ad17949a59b16f8d64e98e82f6d92fe
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-21 17:35:34 -0700

    Update ArrowDiv.js

--- components/ArrowDiv.js
+++ components/ArrowDiv.js
@@ -2,21 +2,17 @@ import { useState } from "react";
 
 export default ArrowDiv = ({ param, currentRoom, setCurrentRoom }) => {
   const [timerActive, setTimerActive] = useState(false);
-  let timerId = setInterval(() => {
-    3000;
-  }, 1000);
+  const intervalValue = (e) => {
+    repeat = setInterval(updateValue(e), 1000);
+  };
   const updateValue = (e) => {
-    setTimerActive("true");
-    let temp = e.target.id;
-    setInterval(() => {
-      if (temp == "decrease") {
-        setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] - 1 });
-      }
-      if (temp == "increase") {
-        setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] + 1 });
-      }
-      console.log("red");
-    }, 1000);
+    if (e.target.id == "decrease") {
+      setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] - 1 });
+    }
+    if (e.target.id == "increase") {
+      setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] + 1 });
+    }
+    console.log("red");
   };
   const stopTimer = () => {
     setInterval(() => {}, 0);
@@ -36,7 +32,7 @@ export default ArrowDiv = ({ param, currentRoom, setCurrentRoom }) => {
       <div
         id="increase"
         className="arrowButton"
-        onMouseDown={(e) => updateValue(e)}
+        onMouseDown={(e) => intervalValue(e)}
         onMouseUp={stopTimer}
       >
         ↑
