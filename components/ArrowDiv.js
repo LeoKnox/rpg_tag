9411f19a84ebe6194e3ae1e4bad2a8d34f8a3dab
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-23 15:35:23 -0700

    Update ArrowDiv.js

--- components/ArrowDiv.js
+++ components/ArrowDiv.js
@@ -4,6 +4,7 @@ export default ArrowDiv = ({ param, currentRoom, setCurrentRoom }) => {
   const [timerActive, setTimerActive] = useState(false);
 
   const start = (e) => {
+    setTimerActive(true);
     let i = 0;
     while (timerActive && i < 10) {
       console.log(e.target.id);
