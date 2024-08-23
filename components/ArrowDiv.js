18ddb7977484951ac05241c6822594f71e3cfaa0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-23 15:25:43 -0700

    Update ArrowDiv.js

--- components/ArrowDiv.js
+++ components/ArrowDiv.js
@@ -3,52 +3,54 @@ import { useState, useEffect } from "react";
 export default ArrowDiv = ({ param, currentRoom, setCurrentRoom }) => {
   const [timerActive, setTimerActive] = useState(false);
 
-  const start = () => {
-    setTimerActive(true);
-  };
-  const updateValue = (e) => {
+  const start = (e) => {
     repeat(() => {
       let i = 0;
       while (timerActive && i < 10) {
-        if (e.target.id == "decrease") {
-          setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] - 1 });
-        }
-        if (e.target.id == "increase") {
-          setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] + 1 });
-        }
+        console.log(e.target.id);
+        updateValue(e.target.id);
+        setTimerActive(true);
         i++;
       }
     });
-    console.log("red");
-  };
-  function repeat(param) {
-    timer = setInterval(param, 1000);
-    param();
-  }
-  const stopTimer = () => {
-    setInterval(() => {}, 0);
-    setTimerActive(false);
-    console.log("stop" + timerActive);
-  };
-  return (
-    <div style={{ display: "flex", color: "darkblue" }}>
-      <div
-        id="decrease"
-        className="arrowButton"
-        onClick={start}
-        onMouseDown={(e) => updateValue(e)}
-        onMouseUp={stopTimer}
-      >
-        ↓
+    const updateValue = (temp) => {
+      console.log("red");
+      if (temp == "decrease") {
+        setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] - 1 });
+      }
+      if (temp == "increase") {
+        setCurrentRoom({ ...currentRoom, [param]: currentRoom[param] + 1 });
+      }
+    };
+    function repeat(param) {
+      timer = setInterval(param, 1000);
+      param();
+    }
+    const stopTimer = () => {
+      setInterval(() => {}, 0);
+      setTimerActive(false);
+      console.log("stop" + timerActive);
+    };
+    return (
+      <div style={{ display: "flex", color: "darkblue" }}>
+        <div
+          id="decrease"
+          className="arrowButton"
+          onClick={start}
+          onMouseDown={(e) => start(e)}
+          onMouseUp={stopTimer}
+        >
+          ↓
+        </div>
+        <div
+          id="increase"
+          className="arrowButton"
+          onMouseDown={(e) => start(e)}
+          onMouseUp={stopTimer}
+        >
+          ↑
+        </div>
       </div>
-      <div
-        id="increase"
-        className="arrowButton"
-        onMouseDown={(e) => updateValue(e)}
-        onMouseUp={stopTimer}
-      >
-        ↑
-      </div>
-    </div>
-  );
+    );
+  };
 };
